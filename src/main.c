560a54873ad32ca313c6b9e5dc3f1d0795391f44
SJC <sjc@debug.online>
2018-11-17 18:45:38 +0800

    inti struct

--- src/main.c
+++ src/main.c
@@ -4,16 +4,15 @@
 #include<string.h>
 
 //define filepath
-#define FILEPATH "D:\\数据结构\\第二次大作业\\src\\weight.csv"
+#define FILEPATH "D:\\weight.csv"
 //define string and construct
-#define String char*
-#define String_MAXSIZE 100
-#define Init_String (String)malloc(sizeof(char)*String_MAXSIZE)
+#define STRING_MAXSIZE 100
+#define INIT_STRING (char*)malloc(sizeof(char)*STRING_MAXSIZE)
 
 typedef struct node Node;
 //Define BinaryTree
 struct node{
-    String Data;
+    char* Data;
     int Weight;
     Node* Lchild;
     Node* Rchild;
@@ -23,12 +22,57 @@ struct node{
 //Define RawData
 typedef struct raw Raw;
 struct raw{
-    String Data;
+    char* Data;
     int Weigth;
 };
 
-Raw*[] readFromFile(){
+char* strReplace(char* src){
+    int i;
+    for(i=0;src[i]!='\0';i++){
+        if(src[i]==','){
+            src[i] = ' ';
+        }
+    }
+    return src;
+}
 
+
+Raw* initRawNode(char* data,int weight){
+    Raw* rw;
+    char* str;
+
+    rw = (Raw*)malloc(sizeof(Raw));
+    str = INIT_STRING;
+    strcpy(str,data);
+    rw->Data = str;
+    rw->Weigth = weight;
+    return rw;
+}
+
+Raw** readFromFile(){
+    FILE* fp;
+    int corr,count;
+    char *temp1,*temp2;
+    int weight;
+    Raw* np;
+    Raw** npp;
+    npp = (Raw**)malloc(sizeof(Raw*)*100);
+    count = 0;
+    fp = fopen(FILEPATH,"r");
+    while(!feof(fp)){
+        temp1 = INIT_STRING;
+        fscanf(fp,"%s",temp1);
+        temp1 = strReplace(temp1);
+        temp2 = INIT_STRING;
+        corr = sscanf(temp1,"%s %d",temp2,&weight);
+        if(corr == 2){
+            np = initRawNode(temp2,weight);
+            npp[count] = np;
+            count++;
+        }    
+    }
+    npp[count] = NULL;
+    return npp;
 }
 
 Node* initEmptyNode(Node* father){
@@ -37,10 +81,10 @@ Node* initEmptyNode(Node* father){
     return np;
 }
 
-Node* initNode(Node* father,String data,int weight){
+Node* initNode(Node* father,char* data,int weight){
     Node* np;
-    String str;
-    str = Init_String;
+    char* str;
+    str = INIT_STRING;
     strcpy(str,data);
     np = initEmptyNode(father);
     np->Data = str;
@@ -52,5 +96,11 @@ Node* initNode(Node* father,String data,int weight){
  main entry fnuction 
 */
 int main(){
-    
+    Raw** npp;
+    int i;
+    npp = readFromFile();
+    for(i=0;npp[i]!=NULL;i++){
+        printf("%s %d\n",npp[i]->Data,npp[i]->Weigth);
+    } 
+    system("pause");
 }
